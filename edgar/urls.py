642b61e28b3670700efc68cae6fdf9041839fc84
Diana <dianavillanueva408@gmail.com>
2023-07-07 17:59:36 +0800

    url templates spacing code

--- edgar/urls.py
+++ edgar/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("hello/", include("finance_data.urls")),
+    path("foo/", include("foo.urls")),
+    path("financial_data/", include("finance_data.urls")),
+    path("admin/", admin.site.urls),
     ]
+
--- finance_data/views.py
+++ finance_data/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
+
+
+
+#def detail(request, question_id):
+#    return HttpResponse("You are looking at the question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
 
 
 def index(request):
-    return HttpResponse("Hello, World.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "financial_data/finance.html", context)
 
-def foo(request):
-    return HttpResponse("Hello,foo.")
 
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "financial_data/detail.html", {"question": question})
